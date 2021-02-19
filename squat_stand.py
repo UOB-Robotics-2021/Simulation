3da5c9a4fdc8fc327e63df33e2393b1b4346dd95
RemiBahar <remibahar@hotmail.com>
2021-02-19 13:53:01 +0000

    Stickman Update
    -Added foot, lower leg and upper log

---
+++ squat_stand.py
@@ -0,0 +1,234 @@
+#Import modules
+import pymunk
+import pygame
+import json
+
+#Custom functions
+def loadConfig(configFile):
+
+    with open(f'{configFile}', 'r') as cfg:
+        config = cfg.read()
+
+    return json.loads(config)
+
+def vector_sum(a1, a2):
+    return [(a1[0]+a2[0]), (a1[1]+a2[1])]
+
+config = loadConfig('config.json')["squatStandConfig"]
+
+#Set-up environment
+space = pymunk.Space()
+space.gravity = 0, config["g"]
+b0 = space.static_body
+
+size = w, h = config["screenSize"]
+fps = 30
+steps = 10
+
+BLACK = (0, 0, 0)
+GRAY = (220, 220, 220)
+WHITE = (255, 255, 255)
+
+#Classes 
+
+class PinJoint:
+    def __init__(self, b, b2, a=(0, 0), a2=(0, 0)):
+        joint = pymunk.PinJoint(b, b2, a, a2)
+        space.add(joint)
+
+
+class PivotJoint:
+    def __init__(self, b, b2, a=(0, 0), a2=(0, 0), collide=True):
+        joint = pymunk.PinJoint(b, b2, a, a2)
+        joint.collide_bodies = collide
+        space.add(joint)
+
+
+class SlideJoint:
+    def __init__(self, b, b2, a=(0, 0), a2=(0, 0), min=50, max=100, collide=True):
+        joint = pymunk.SlideJoint(b, b2, a, a2, min, max)
+        joint.collide_bodies = collide
+        space.add(joint)
+
+
+class GrooveJoint:
+    def __init__(self, a, b, groove_a, groove_b, anchor_b):
+        joint = pymunk.GrooveJoint(
+            a, b, groove_a, groove_b, anchor_b)
+        joint.collide_bodies = False
+        space.add(joint)
+
+
+class DampedRotarySpring:
+    def __init__(self, b, b2, angle, stiffness, damping):
+        joint = pymunk.DampedRotarySpring(
+            b, b2, angle, stiffness, damping)
+        space.add(joint)
+
+
+class RotaryLimitJoint:
+    def __init__(self, b, b2, min, max, collide=True):
+        joint = pymunk.RotaryLimitJoint(b, b2, min, max)
+        joint.collide_bodies = collide
+        space.add(joint)
+
+
+class RatchetJoint:
+    def __init__(self, b, b2, phase, ratchet):
+        joint = pymunk.GearJoint(b, b2, phase, ratchet)
+        space.add(joint)
+
+
+class SimpleMotor:
+    def __init__(self, b, b2, rate):
+        joint = pymunk.SimpleMotor(b, b2, rate)
+        space.add(joint)
+
+
+class GearJoint:
+    def __init__(self, b, b2, phase, ratio):
+        joint = pymunk.GearJoint(b, b2, phase, ratio)
+        space.add(joint)
+
+
+class Segment:
+    def __init__(self, p0, v, m=10, radius=2):
+        self.body = pymunk.Body()
+        self.body.position = p0
+        shape = pymunk.Segment(self.body, (0, 0), v, radius)
+        shape.mass = m
+        shape.density = 0.1
+        shape.elasticity = 0.5
+        shape.filter = pymunk.ShapeFilter(group=1)
+        shape.color = (0, 255, 0, 0)
+        space.add(self.body, shape)
+
+
+class Circle:
+    def __init__(self, pos, radius=20):
+        self.body = pymunk.Body()
+        self.body.position = pos
+        shape = pymunk.Circle(self.body, radius)
+        shape.density = 0.01
+        shape.friction = 0.5
+        shape.elasticity = 1
+        space.add(self.body, shape)
+
+
+class Box:
+    def __init__(self, p0=(0, 0), p1=(w, h), d=4):
+        x0, y0 = p0
+        x1, y1 = p1
+        pts = [(x0, y0), (x1, y0), (x1, y1), (x0, y1)]
+        for i in range(4):
+            segment = pymunk.Segment(
+                space.static_body, pts[i], pts[(i+1) % 4], d)
+            segment.elasticity = 1
+            segment.friction = 0.5
+            space.add(segment)
+
+
+class Poly:
+    def __init__(self, pos, vertices):
+        self.body = pymunk.Body(1, 100)
+        self.body.position = pos
+
+        shape = pymunk.Poly(self.body, vertices)
+        shape.filter = pymunk.ShapeFilter(group=1)
+        shape.density = 0.01
+        shape.elasticity = 0.5
+        shape.color = (255, 0, 0, 0)
+        space.add(self.body, shape)
+
+
+class Rectangle:
+    def __init__(self, pos, size=(80, 50)):
+        self.body = pymunk.Body()
+        self.body.position = pos
+
+        shape = pymunk.Poly.create_box(self.body, size)
+        shape.density = 0.1
+        shape.elasticity = 1
+        shape.friction = 1
+        space.add(self.body, shape)
+
+
+class App:
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.screen = pygame.display.set_mode(size)
+        self.draw_options = DrawOptions(self.screen)
+        self.running = True
+        self.gif = 0
+        self.images = []
+
+    def run(self):
+        while self.running:
+            for event in pygame.event.get():
+                self.do_event(event)
+ 
+            self.draw()
+            self.clock.tick(fps)
+
+            for i in range(steps):
+                space.step(1/fps/steps)
+
+        pygame.quit()
+
+    def do_event(self, event):
+        if event.type == QUIT:
+            self.running = False
+        keys = pygame.key.get_pressed()
+        
+        if keys[pygame.K_UP]: 
+            print(dir(lower_leg.body))
+            print(foot.body.position)
+            #print(dir(leg.body))
+            #leg.body.apply_impulse_at_local_point([10000,0], leg.body.position)
+            upper_leg.body.apply_impulse_at_local_point([10000,0], upper_leg.body.position)
+        if event.type == KEYDOWN:
+            if event.key in (K_q, K_ESCAPE):
+                self.running = False
+
+            elif event.key == K_p:
+                pygame.image.save(self.screen, 'joint.png')
+
+            elif event.key == K_g:
+                self.gif = 60
+        
+
+    def draw(self):
+        self.screen.fill(GRAY)
+        space.debug_draw(self.draw_options)
+        pygame.display.update()
+
+        text = f'fpg: {self.clock.get_fps():.1f}'
+        pygame.display.set_caption(text)
+        self.make_gif()
+
+    def make_gif(self):
+        if self.gif > 0:
+            strFormat = 'RGBA'
+            raw_str = pygame.image.tostring(self.screen, strFormat, False)
+            image = Image.frombytes(
+                strFormat, self.screen.get_size(), raw_str)
+            self.images.append(image)
+            self.gif -= 1
+            if self.gif == 0:
+                self.images[0].save('joint.gif',
+                                    save_all=True, append_images=self.images[1:],
+                                    optimize=True, duration=1000//fps, loop=0)
+                self.images = []
+
+#Code to generate figure
+            
+foot = Segment(config["anklePosition"], config["footVector"], config["footMass"])
+lower_leg = Segment(config["anklePosition"], config["lowerLegVector"], config["lowerLegMass"])
+ankle = PivotJoint(foot.body, lower_leg.body, (0,0))
+knee_position = vector_sum(config["anklePosition"], config["lowerLegVector"])
+upper_leg = Segment(knee_position, config["upperLegVector"], config["upperLegMass"])
+knee = PivotJoint(lower_leg.body, upper_leg.body, config["lowerLegVector"])
+#print(list(foot.body.shapes)[0]) #Access foot shape
+
+App().run()
